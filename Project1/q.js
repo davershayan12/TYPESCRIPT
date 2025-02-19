e8b9b8b42cf596c269db48adc3914c1136a2d5e0
davershayan12 <davershayan13@gmail.com>
2025-02-20 01:24:55 +0500

    .

--- Project1/q.js
+++ Project1/q.js
@@ -1,2 +1,46 @@
-var a = 3;
-console.log(a);
+function gcd(a, b) {
+    var answer = 1;
+    var val1 = a;
+    var val2 = b;
+    var hcf1 = [];
+    var hcf2 = [];
+    var GCD = [];
+    var i = 1;
+    while (i < val1) {
+        i++;
+        if (val1 % i === 0) {
+            hcf1.push(i);
+            val1 = val1 / i;
+            i = 1;
+            if (val1 == 1) {
+                break;
+            }
+        }
+    }
+    i = 1;
+    while (i < val2) {
+        i++;
+        if (val2 % i === 0) {
+            hcf2.push(i);
+            val2 = val2 / i;
+            i = 1;
+            if (val2 == 1) {
+                break;
+            }
+        }
+    }
+    for (var i_1 = 0; i_1 < hcf1.length; i_1++) {
+        for (var j = 0; j < hcf2.length; j++) {
+            if (hcf1[i_1] === hcf2[j]) {
+                GCD.push(hcf1[i_1]);
+                hcf2.splice(j, 1);
+                break;
+            }
+        }
+    }
+    for (var i_2 = 0; i_2 < GCD.length; i_2++) {
+        answer *= GCD[i_2];
+    }
+    return "GCD of ".concat(a, " and ").concat(b, " is ").concat(answer, " ");
+}
+console.log(gcd(35, 14));
--- Project1/q.ts
+++ Project1/q.ts
@@ -1,2 +1,49 @@
-let a:number=3;
-console.log(a)
+function gcd (a:number, b:number){
+    let answer: number =1;
+    let val1: number = a;
+    let val2: number = b;
+    let hcf1: number[]=[];
+    let hcf2: number[]=[];
+    let GCD : number[] =[];
+    let i: number =1
+    while(i< val1){
+        i++
+        if (val1%i === 0) {
+            hcf1.push(i);
+            val1=val1/i;
+            i=1;
+            if (val1 == 1) {
+                break;
+            }
+        }     
+    }
+    i=1
+    while(i< val2){
+        i++
+        if (val2%i === 0) {
+            hcf2.push(i);
+            val2=val2/i;
+            i=1;
+            if (val2 == 1) {
+                break;
+            }
+        }     
+    }
+    for (let i = 0; i < hcf1.length; i++) {
+        for (let j = 0; j < hcf2.length; j++) {
+            if (hcf1[i] === hcf2[j]) {
+                GCD.push(hcf1[i]);
+                hcf2.splice(j, 1);
+                break;
+            }
+        }
+    }
+
+    for (let i = 0; i < GCD.length; i++) {
+        answer*=GCD[i];
+    }
+
+    return `GCD of ${a} and ${b} is ${answer} `;
+}
+
+console.log(gcd(35,14))
